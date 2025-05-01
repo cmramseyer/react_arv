bf676d20ddfc27c7d08453b6386f64b3ff23a03f
carlos <cmramseyer@gmail.com>
2025-05-01 16:19:45 -0300

    shadcn working

--- src/pages/OrdenesFumigacion.jsx
+++ src/pages/OrdenesFumigacion.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { getOrdenesFumigacion } from '../services/ordenesFumigacionService'
 import { Link, useNavigate } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
+
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
 
 export default function OrdenesFumigacion() {
   const [ordenes, setOrdenes] = useState([])
@@ -33,6 +37,17 @@ export default function OrdenesFumigacion() {
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold">Órdenes de Fumigación</h2>
         <div>
+        <Button variant="secondary">Botón secundario</Button>
+        <Button variant="default">Botón default</Button>
+        <Button variant="outline">Botón outline</Button>
+        <Button variant="link">Botón link</Button>
+        <Button variant="ghost">Botón ghost</Button>
+        <div>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <Label htmlFor="terms">Accept terms and conditions</Label>
+          </div>
+        </div>
           <button
             onClick={() => handleEstadoOrdenes('')}
             className={seleccionadoClass('')}
