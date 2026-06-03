e6173102e94fde04bf7be8de2a46eff7c153e4d8
carlos <cmramseyer@gmail.com>
2026-06-03 16:06:58 -0300

    more tests for AdjuntoList.test

--- src/tests/AdjuntosList.test.jsx
+++ src/tests/AdjuntosList.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within, waitFor } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom'
 
 import { AdjuntosList } from '@/components/AdjuntosList'
-
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -31,7 +31,13 @@ const renderWithQueryClient = (ui, queryClient = createQueryClient()) => {
   };
 };
 
+let user
+
 describe('Adjuntos list', () => {
+
+  beforeEach(()=> {
+    user = userEvent.setup()
+  })
 
   it('renders adjuntos', async () => {
 
@@ -45,4 +51,41 @@ describe('Adjuntos list', () => {
     expect(image).toHaveAttribute("src", expect.stringContaining("localhost:3000/uploads/plano-lote-uno.png"));
   })
 
+  it('open modal to display image', async () => {
+
+    renderWithQueryClient(
+      <MemoryRouter>
+        <AdjuntosList loteId={'1'} />
+      </MemoryRouter>
+    )
+
+    const images = await screen.findAllByRole('img', { name: /adjunto/i })
+
+    await user.click(images[0])
+    expect(await screen.getByText("Imagen")).toBeInTheDocument()
+  })
+
+  it('open modal, display image, close modal', async () => {
+
+    renderWithQueryClient(
+      <MemoryRouter>
+        <AdjuntosList loteId={'1'} />
+      </MemoryRouter>
+    )
+
+    const images = await screen.findAllByRole('img', { name: /adjunto/i })
+
+    await user.click(images[0])
+    expect(await screen.getByText("Imagen")).toBeInTheDocument()
+
+    const dialog = await screen.findByRole("dialog");
+    expect(dialog).toBeInTheDocument();
+
+    const closeButton = within(dialog).getByRole("button", { name: /cerrar/i });
+    await user.click(closeButton);
+
+    await waitFor(() => {
+      expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    });
+  })
 })
