c9e2adf0c165471301c554d175a48f4174e6c7b6
carlos <cmramseyer@gmail.com>
2026-06-24 13:35:16 -0300

    wrap main with TooltipProvider

--- src/main.jsx
+++ src/main.jsx
@@ -31,44 +31,47 @@ import MaquinistaNew from '@/features/maquinistas/pages/MaquinistaNew'
 import MaquinistaEdit from '@/features/maquinistas/pages/MaquinistaEdit'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/estancias" element={<Estancias />} />
-            <Route path="/estancias/new" element={<EstanciaNew />} />
-            <Route path="/estancias/:id/edit" element={<EstanciaEdit />} />
-            <Route path="/cultivos" element={<Cultivos />} />
-            <Route path="/cultivos/new" element={<CultivoNew />} />
-            <Route path="/cultivos/:id/edit" element={<CultivoEdit />} />
-            <Route path="/maquinistas" element={<Maquinistas />} />
-            <Route path="/maquinistas/new" element={<MaquinistaNew />} />
-            <Route path="/maquinistas/:id/edit" element={<MaquinistaEdit />} />
-            <Route path="/lotes" element={<Lotes />} />
-            <Route path="/lotes/:id" element={<LoteShow />} />
-            <Route path="/lotes/new" element={<LoteNew />} />
-            <Route path="/lotes/:id/edit" element={<LoteEdit />} />
-            <Route path="/productos" element={<Productos />} />
-            <Route path="/productos/new" element={<ProductoNew />} />
-            <Route path="/productos/:id/edit" element={<ProductoEdit />} />
-            <Route path="/estadisticas" element={<Estadistica />} />
-            <Route path="/ordenes_fumigacion" element={<OrdenesFumigacion />} />
-            <Route path="/buscar_ordenes" element={<BuscarOrden />} />
-            <Route path="/ordenes_fumigacion/new" element={<OrdenFumigacionNew />} />
-            <Route path="/ordenes_fumigacion/pendiente_factura" element={<Facturacion />} />
-            <Route path="/ordenes_fumigacion/:id" element={<OrdenFumigacionShow />} />
-            <Route path="/ordenes_fumigacion/:id/edit" element={<OrdenFumigacionEdit />} />
-            <Route path="/ordenes_fumigacion/:id/terminar" element={<OrdenFumigacionTerminar />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <TooltipProvider>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/estancias" element={<Estancias />} />
+              <Route path="/estancias/new" element={<EstanciaNew />} />
+              <Route path="/estancias/:id/edit" element={<EstanciaEdit />} />
+              <Route path="/cultivos" element={<Cultivos />} />
+              <Route path="/cultivos/new" element={<CultivoNew />} />
+              <Route path="/cultivos/:id/edit" element={<CultivoEdit />} />
+              <Route path="/maquinistas" element={<Maquinistas />} />
+              <Route path="/maquinistas/new" element={<MaquinistaNew />} />
+              <Route path="/maquinistas/:id/edit" element={<MaquinistaEdit />} />
+              <Route path="/lotes" element={<Lotes />} />
+              <Route path="/lotes/:id" element={<LoteShow />} />
+              <Route path="/lotes/new" element={<LoteNew />} />
+              <Route path="/lotes/:id/edit" element={<LoteEdit />} />
+              <Route path="/productos" element={<Productos />} />
+              <Route path="/productos/new" element={<ProductoNew />} />
+              <Route path="/productos/:id/edit" element={<ProductoEdit />} />
+              <Route path="/estadisticas" element={<Estadistica />} />
+              <Route path="/ordenes_fumigacion" element={<OrdenesFumigacion />} />
+              <Route path="/buscar_ordenes" element={<BuscarOrden />} />
+              <Route path="/ordenes_fumigacion/new" element={<OrdenFumigacionNew />} />
+              <Route path="/ordenes_fumigacion/pendiente_factura" element={<Facturacion />} />
+              <Route path="/ordenes_fumigacion/:id" element={<OrdenFumigacionShow />} />
+              <Route path="/ordenes_fumigacion/:id/edit" element={<OrdenFumigacionEdit />} />
+              <Route path="/ordenes_fumigacion/:id/terminar" element={<OrdenFumigacionTerminar />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 )
