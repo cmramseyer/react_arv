562fb7de17974e3609919587502ef9f80e172fa1
carlos <cmramseyer@gmail.com>
2026-07-01 20:05:20 -0300

    fix import

--- src/features/productos/tests/ProductoList.test.jsx
+++ src/features/productos/tests/ProductoList.test.jsx
@@ -4,7 +4,7 @@ import { userEvent } from '@testing-library/user-event'
 import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import ProductoList from '@/components/ProductoList'
+import ProductoList from '../components/ProductoList'
 
 import { setupServer } from 'msw/node'
 import { productoHandlers } from '@/features/productos/mocks/productoHandlers'
